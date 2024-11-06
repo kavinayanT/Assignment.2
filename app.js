5cea8de7d317f7a1d475eab14c4bcca57f6261bb
kavinayanT <kavinayan.thayananthakuamr@ontariotechu.net>
2024-11-05 22:52:09 -0500

    Initial commit with Express app and EJS layout

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const app = express();
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
-// Use express-ejs-layouts
+// Use express-ejs-layouts for layout support
 app.use(ejsLayouts);
 
-// Define the views folder
+// Define the views folder (where your EJS files will reside)
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the "public" folder (CSS, JS, images, etc.)
@@ -36,7 +36,7 @@ app.get('/contact', (req, res) => {
   res.render('contact', { title: 'Contact Me' });
 });
 
-// Start the server
+// Start the server on the specified port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
